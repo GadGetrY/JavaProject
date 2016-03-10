f20e281e90ff5c5b396bea896f1b5ad30653c0ee
GadGetrY <juri.deisling@web.de>
2016-03-10 10:32:35 +0100

    Update scenarioTest.java

--- scenarioTest.java
+++ scenarioTest.java
@@ -11,18 +11,5 @@ public class scenarioTest {
 		fail("Not yet implemented");
 	}
 	
-	@Test
-	public void testPopulateAuctions(){
-	//assertEquals(QueryHandler.INSTANCE.;
-	}
-	
-	@Test
-	public void testFindAllAuctionUsers() {
-	//assertEquals(QueryHandler.INSTANCE.findAuctionUserScenario().size(),5);
-	}
-	
-	@Test
-	public void testFindUserById() {
-	//assertEquals(QueryHandler.INSTANCE.findAuctionUserByIdScenario(jpaAuctionUserDao, em)3).getAuctionUserId(),3);
-	}
+
 }
